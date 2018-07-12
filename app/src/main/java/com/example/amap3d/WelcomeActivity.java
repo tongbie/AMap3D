576dc81f3f5749f6d5beb9e6390548ada1b8ed76
tongbie <2489550615@qq.com>
2018-07-13 00:11:15 +0800

    2018/7/13 权限组申请

--- app/src/main/java/com/example/amap3d/WelcomeActivity.java
+++ app/src/main/java/com/example/amap3d/WelcomeActivity.java
@@ -3,6 +3,7 @@ package com.example.amap3d;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Matrix;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -11,32 +12,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestPermissions();
+    }
+
+    private List<String> permissionList;
+
+    private void requestPermissions() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions();
+            permissionList = new ArrayList<>();
+            if (ContextCompat.checkSelfPermission(WelcomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if (ContextCompat.checkSelfPermission(WelcomeActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
+            if (permissionList.isEmpty()) {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                finish();
+            } else {
+                requestPermissions(permissionList.toArray(new String[permissionList.size()]), 0x000);
+            }
         } else {
             startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
             finish();
-        }
-    }
-
-    private final int ACCESS_FINE_LOCATION = 0x001;
-    private final int WRITE_EXTERNAL_STORAGE = 0x002;
-
-    private void requestPermissions() {
-        if (ContextCompat.checkSelfPermission(WelcomeActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(WelcomeActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION);
-        } else {
-            if (ContextCompat.checkSelfPermission(WelcomeActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(WelcomeActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_EXTERNAL_STORAGE);
-            } else {
-                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
-                finish();
-            }
         }
     }
 
@@ -44,25 +50,18 @@ public class WelcomeActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
-            case ACCESS_FINE_LOCATION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ContextCompat.checkSelfPermission(WelcomeActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(WelcomeActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_EXTERNAL_STORAGE);
-                    }else {
-                        startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
-                        finish();
+            case 0x000:
+                boolean isAllPermissionAgreed = true;
+                for (int i = 0; i < permissions.length; i++) {
+                    if (ContextCompat.checkSelfPermission(WelcomeActivity.this, permissions[i]) != PackageManager.PERMISSION_GRANTED) {
+                        isAllPermissionAgreed = false;
                     }
-                } else {
-                    Toast.makeText(getApplicationContext(), "未获得位置信息权限，程序无法正常使用", Toast.LENGTH_LONG).show();
-                    finish();
                 }
-                break;
-            case WRITE_EXTERNAL_STORAGE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (isAllPermissionAgreed) {
                     startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
                     finish();
-                } else {
-                    Toast.makeText(WelcomeActivity.this, "未获得存储权限，程序无法正常使用", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(WelcomeActivity.this, "未获得全部权限，无法使用该功能", Toast.LENGTH_SHORT).show();
                     finish();
                 }
                 break;
